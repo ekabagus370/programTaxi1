201e0e42e329f0e49f4cc9d669b7981deda18956
ekabagus370 <ekabaguserlangga370@amikom.ac.id>
2022-03-23 22:02:39 +0700

    Taxi

---
+++ Taxi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            // penentuan nilai properties
+            taxi.DriverName = "Eka Bagus Erlangga";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 8;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DroppOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
